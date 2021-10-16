c5fa7abcbd48c4f5e38b8991d5fcaa082fd34d9a
pyorolong <38350610+pyorolong@users.noreply.github.com>
2021-10-17 01:29:03 +0900

    ViewModel 클래스 추가

---
+++ app/src/main/java/com/cont96roller/library/viewmodel/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.cont96roller.library.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+}
